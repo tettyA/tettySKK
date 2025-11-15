26ea2f799367ea1a0575371f31ecd99985954ef8
tettyA <tettyA@outlook.jp>
2025-11-15 23:11:47 +0900

    一文字だけ漢字に変換出来るように(暫定辞書に登録しているものに限る)

--- tettySKK/CInsertTextEditSession.cpp
+++ tettySKK/CInsertTextEditSession.cpp
@@ -56,3 +56,66 @@ STDMETHODIMP_(ULONG) CInsertTextEditSession::Release() {
 	}
 	return _refCount;
 }
+
+
+
+CGetTextEditSession::CGetTextEditSession(ITfRange* pRange, std::wstring* pOutput)
+{
+	_refCount = 1;
+	_pRange = pRange;
+	_pOutput = pOutput;
+
+
+}
+CGetTextEditSession::~CGetTextEditSession()
+{
+	_pRange.Release();
+	_pOutput = nullptr;
+}
+
+STDMETHODIMP CGetTextEditSession::QueryInterface(REFIID riid, void** ppvObj)
+{
+	if (ppvObj == nullptr)
+	{
+		return E_INVALIDARG;
+	}
+	ppvObj = nullptr;
+	if (IsEqualIID(riid, IID_IUnknown) || IsEqualIID(riid, IID_ITfEditSession)) {
+		*ppvObj = static_cast<ITfEditSession*>(this);
+	}
+	if (*ppvObj) {
+		AddRef();
+		return S_OK;
+	}
+	return E_NOINTERFACE;
+}
+
+
+STDMETHODIMP_(ULONG) CGetTextEditSession::AddRef()
+{
+	return ++_refCount;
+}
+STDMETHODIMP_(ULONG) CGetTextEditSession::Release()
+{
+	if (--_refCount == 0) {
+		delete this;
+		return 0;
+	}
+	return _refCount;
+}
+
+STDMETHODIMP_(HRESULT __stdcall) CGetTextEditSession::DoEditSession(TfEditCookie ec)
+{
+	WCHAR buffer[256];
+	ULONG cchFetched = 0;
+
+	if (SUCCEEDED(_pRange->GetText(ec, 0, buffer, 256, &cchFetched)))
+	{
+		if (cchFetched > 0) {
+			_pOutput->assign(buffer, cchFetched);
+		}
+	}
+
+	return S_OK;
+}
+
--- tettySKK/CInsertTextEditSession.h
+++ tettySKK/CInsertTextEditSession.h
@@ -1,8 +1,10 @@
 #pragma once
 #include <msctf.h>
 #include "CSkkIme.h"
+#include <string>
 
 //ref https://github.com/nathancorvussolis/corvusskk/blob/2904b3ad7ba80e66e717aef6805164c74fcec71d/imcrvtip/EditSession.h#L6
+//テキスト挿入のためのEditSession
 class CInsertTextEditSession :
     public ITfEditSession
 {
@@ -25,3 +27,20 @@ private:
 	BOOL _isDetermined;
 };
 
+//Compositionのテキストを取得するためのEditSession
+class CGetTextEditSession :public ITfEditSession
+{
+public:
+	CGetTextEditSession(ITfRange* pRange, std::wstring* pOutput);
+	~CGetTextEditSession();
+	//IUnknown methods
+	STDMETHODIMP QueryInterface(REFIID riid, void** ppvObj);
+	STDMETHODIMP_(ULONG) AddRef();
+	STDMETHODIMP_(ULONG) Release();
+	//ITfEditSession methods
+	STDMETHODIMP DoEditSession(TfEditCookie ec);
+private:
+	LONG _refCount;
+	CComPtr<ITfRange> _pRange;
+	std::wstring* _pOutput;
+};
